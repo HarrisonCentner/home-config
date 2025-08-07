1a32d77e639f60c598273ef2c1cac09d8f5a3f55
HarrisonCentner <harrison.centner@gmail.com>
2025-08-06 23:02:10 -0700

    use upstream nix instead of determinate nix

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.11";
+      url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
@@ -16,9 +16,17 @@
     let
     configuration = { pkgs, ... }: {
       # Necessary for using flakes on this system.
-      nix.settings.experimental-features = "nix-command flakes";
-      # This will allow you to use nix-darwin with Determinate. Some nix-darwin functionality that relies on managing the Nix installation, like the `nix.*` options to adjust Nix settings or configure a Linux builder, will be unavailable.
-      nix.enable = false;
+      nix.settings.experimental-features = [ "nix-command" "flakes" ];
+      # Binary Cache for haskell.nix
+      nix.settings.trusted-public-keys = [
+        "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
+        "ryantrinkle.com-1:JJiAKaRv9mWgpVAz8dwewnZe0AzzEAzPkagE9SP5NWI="
+        "reflex-frp.org-1:4F+Vg8pNi+n9EWTyUd9T2DGMo2HgPKgwZdUJiknHTdc="
+      ];
+      nix.settings.substituters = [
+        "https://cache.iog.io"
+        "https://nixcache.reflex-frp.org"
+      ];
 
       # The platform the configuration will be used on.
       nixpkgs.hostPlatform = "x86_64-darwin";
